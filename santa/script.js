4fb995550f1c5796d7b4206e6d46559a81348b99
felix bintang elisha <71473306+frominside12@users.noreply.github.com>
2024-11-24 16:29:54 +0700

    Add files via upload

---
+++ santa/script.js
@@ -0,0 +1,5 @@
+document.getElementById('revealButton').addEventListener('click', function () {
+    const message = document.getElementById('hiddenMessage');
+    message.style.display = 'block';
+    alert("HBD Y BUBUB!!!!!!!!!!");
+});
